1cc873990832a855dec171bf7a109190b3ded2fb
Your Name <user@example.com>
2025-06-24 10:12:50 +0700

    add comment and like to feginclient

--- src/main/java/com/f4/gateway/config/WebConfigurer.java
+++ src/main/java/com/f4/gateway/config/WebConfigurer.java
@@ -48,6 +48,7 @@ public class WebConfigurer implements WebFluxConfigurer {
             source.registerCorsConfiguration("/*/api/**", config);
             source.registerCorsConfiguration("/services/*/api/**", config);
             source.registerCorsConfiguration("/*/management/**", config);
+            source.registerCorsConfiguration("/oauth2/**", config);
         }
         return source;
     }
