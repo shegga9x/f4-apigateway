69807e69a8b5e54f8d40f02918cf64c5bd5975e2
Your Name <user@example.com>
2025-07-01 19:17:04 +0700

    init new vps

--- src/main/java/com/f4/gateway/DevGateway.java
+++ src/main/java/com/f4/gateway/DevGateway.java
@@ -46,7 +46,7 @@ public class DevGateway {
      * Initializes apiGateway.
      * <p>
      * Spring profiles can be configured with a program argument
-     * --spring.profiles.active=your-active-profile
+     * --spring.profiles.activse=your-active-profile
      * <p>
      * You can find more information on how profiles work with JHipster on <a href=
      * "https://www.jhipster.tech/profiles/">https://www.jhipster.tech/profiles/</a>.
